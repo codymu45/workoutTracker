c93afefa55c905c8d074670dc380e1c342c22a71
Cody Mullen <mullen.cody@yahoo.com>
2020-12-01 21:47:46 -0800

    adding working html routes

---
+++ models/Workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          required: "Enter an Exercise Type"
+        },
+        name: {
+          type: String,
+          required: "Enter Exercise Name"
+        },
+        duration: {
+          type: Number,
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      }
+    ]
+  });
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
+
--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require("express");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000;
+const path = require("path");
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
+// const db = require("/config/key").MongoURI;
+
+// app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+// connect to mongodb
+mongoose.connect("mongodb://localhost:27017/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
-});
+}).then(() => console.log("Connected to Mongodb"))
+.catch((err) => console.log(err));
 
-// routes
-app.use(require("./routes/api.js"));
+require("./routes/html-routes.js")(app);
+// require("./routes/api-routes.js")(app);
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on ${PORT}!`);
 });
